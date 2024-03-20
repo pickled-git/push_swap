d37cc171a5529257d139f6d775b22b58374d40cf
oprosvir <ol7897@ya.ru>
2024-03-20 17:51:21 +0100

    norm fix

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@sudent.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 22:13:00 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/03/18 22:16:09 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/03/20 17:49:57 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 #include <stdlib.h>
 
 // Define a struct for stack nodes
-typedef struct  s_node
+typedef struct s_node
 {
-    int value;
-    struct s_node *next;
-}               t_node;
+	int				value;
+	struct s_node	*next;
+}					t_node;
 
 // Function prototypes for stack operations
-void push(t_node **stack, int value);
-int pop(t_node **stack);
-void swap(t_node **stack);
-void rotate(t_node **stack);
-void reverse_rotate(t_node **stack);
+void				push(t_node **stack, int value);
+int					pop(t_node **stack);
+void				swap(t_node **stack);
+void				rotate(t_node **stack);
+void				reverse_rotate(t_node **stack);
