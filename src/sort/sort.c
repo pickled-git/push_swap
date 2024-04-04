d7b7b971ce61a0d1934d2e768f30ef14b0983065
oprosvir <ol7897@ya.ru>
2024-04-04 23:25:04 +0200

    simple sort up to 5 elements

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,38 +6,95 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 15:19:11 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/04/02 02:43:27 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/04/04 23:21:55 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
 
-void	sort_three(t_stack **stack_a)
+void	sort_three(t_stack **a)
 {
 	int	first;
 	int	second;
 	int	third;
 
-	first = (*stack_a)->value;
-	second = (*stack_a)->next->value;
-	third = (*stack_a)->next->next->value;
+	first = (*a)->value;
+	second = (*a)->next->value;
+	third = (*a)->next->next->value;
 	if (first > second && first < third)
-		sa(stack_a);
+		sa(a);
 	else if (first > second && second > third)
 	{
-		sa(stack_a);
-		rra(stack_a);
+		sa(a);
+		rra(a);
 	}
 	else if (first > second && second < third)
-		ra(stack_a);
+		ra(a);
 	else if (first < second && first > third)
-		rra(stack_a);
+		rra(a);
 	else if (first < third && second > third)
 	{
-		sa(stack_a);
-		ra(stack_a);
+		sa(a);
+		ra(a);
 	}
+}
+
+int	find_min_index(t_stack *a)
+{
+	int		min_index;
+	int		index;
+	int		min_value;
+	t_stack	*current;
+
+	min_index = 0;
+	index = 0;
+	min_value = a->value;
+	current = a;
+	while (current)
+	{
+		if (current->value < min_value)
+		{
+			min_value = current->value;
+			min_index = index;
+		}
+		current = current->next;
+		index++;
+	}
+	return (min_index);
+}
+
+void	push_min_to_b(t_stack **a, t_stack **b)
+{
+	int	min_index;
+
+	min_index = find_min_index(*a);
+	if (min_index <= stack_size(*a) / 2)
+	{
+		while (min_index--)
+			ra(a);
+	}
+	else
+	{
+		while (min_index++ < stack_size(*a))
+			rra(a);
+	}
+	pb(a, b);
+}
+
+void	simple_sort(t_stack **a, t_stack **b)
+{
+	int	size;
+
+	size = stack_size(*a);
+	while (size > 3)
+	{
+		push_min_to_b(a, b);
+		size--;
+	}
+	sort_three(a); // Сортировка трех оставшихся элементов в стеке 'a'
+	while (*b)
+		pa(a, b); // Перемещаем элементы обратно в 'a'
 }
 
 void	sort_stack(t_stack **stack_a)
@@ -49,8 +106,8 @@ void	sort_stack(t_stack **stack_a)
 	size = stack_size(*stack_a);
 	if (size == 2)
 		sa(stack_a);
-	else if (size == 3)
-		sort_three(stack_a);
+	else if (size <= 5)
+		simple_sort(stack_a, &stack_b);
 	else
 	{
 		assign_indexes(stack_a);
